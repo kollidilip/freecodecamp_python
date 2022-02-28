5a30dac3de21f3d1238767e077a5a0099b07d7c0
dilipkolli <dilipkolli.92@gmail.com>
2022-02-28 14:23:45 +0530

    first three lines draft

--- Arithmetic_formatter/arithmetic_arranger.py
+++ Arithmetic_formatter/arithmetic_arranger.py
@@ -1,11 +1,64 @@
-def arithmetic_arranger(problems):
-    
+def arithmetic_arranger(problems,*args):    
+
     # problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+    isvalid = problem_validator(problems)
+
+    if isvalid:
+        firstLine = []
+        secondLine = []
+        thirdline = []
+        resultLine = []
+
+        for prob in problems:
+            if ('+' in prob):
+                qsplit = prob.split('+')
+                
+                num1 = qsplit[0].strip()
+                num2 = qsplit[1].strip()
+                diffLen = max(len(num1),len(num2)) - min(len(num1),len(num2))
+
+                if len(num1) > len(num2):
+                    firstLine.append('  '+num1)
+                    secondLine.append('+ '+' '*diffLen+num2)
+                else:
+                    firstLine.append('  '+' '*diffLen+num1)
+                    secondLine.append('+ '+num2)
+                
+                thirdline.append('--'+'-'*max(len(num1),len(num2)))
+
+            else:
+
+                qsplit = prob.split('-')
+
+                num1 = qsplit[0].strip()
+                num2 = qsplit[1].strip()
+                diffLen = max(len(num1),len(num2)) - min(len(num1),len(num2))
+                
+                if len(num1) > len(num2):
+                    firstLine.append('  '+num1)
+                    secondLine.append('- '+' '*diffLen+num2)
+                else:
+                    firstLine.append('  '+' '*diffLen+num1)
+                    secondLine.append('- '+num2)
+
+                thirdline.append('--'+'-'*max(len(num1),len(num2)))
+
+    if args:
+        print(args)
+    else:
+        print(firstLine,secondLine,thirdline,resultLine)
     
+
+    return 'end'
+
+def problem_validator(problems):
+    isvalid = True
+
     # If there are too many problems supplied to the function. The limit is five, anything more will return:
     # "Error: Too many problems."
     if len(problems) >= 5:
         print('Error: Too many problems.')
+        isvalid = False
     else:
         # The appropriate operators the function will accept are addition and subtraction.
         # Multiplication and division will return an error.
@@ -14,6 +67,7 @@ def arithmetic_arranger(problems):
         for prob in problems:
             if (('+' not in prob) and ('-' not in prob)):
                 print("Error: Operator must be '+' or '-'.")
+                isvalid = False
                 break
             else:
                 
@@ -22,28 +76,27 @@ def arithmetic_arranger(problems):
                 
                 if ('+' in prob):
                     qsplit = prob.split('+')
-                    check_ifnumbers(qsplit)
+                    isvalid = check_ifnumbers(qsplit)
                 else:
                     qsplit = prob.split('-')
-                    check_ifnumbers(qsplit)
-                    
-
-                    
-                    
-                
-
-
-
-    return 'end'
-
+                    isvalid = check_ifnumbers(qsplit)
+        return isvalid
 
 def check_ifnumbers(qsplit):
+    is_valid = True
     for ques_ in qsplit:
         if not ques_.strip().isnumeric():
             print("Error: Numbers must only contain digits.")
-
-            
-            
+            is_valid = False
+            break
+        else:
+        # Each operand (aka number on each side of the operator) has a max of four digits in width. Otherwise, the error string returned will be:
+        # Error: Numbers cannot be more than four digits.
+            if len(ques_.strip()) > 4:
+                print("Error: Numbers cannot be more than four digits.")
+                is_valid = False
+                break
+    return is_valid
             
         
  
